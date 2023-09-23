8f6b2f829a13ab70b65ca1a0fbeb7feac4db9237
FreecouS_BG <63102871+Slavamk3dell@users.noreply.github.com>
2023-09-23 14:02:10 +0300

    add name
    Добавил запрос имени

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Slava!!!");
+Console.Write("Введите имя: ");
+var name = Console.ReadLine();
+
+Console.WriteLine($"Привет, {name}!!!");
