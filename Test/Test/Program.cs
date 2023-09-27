0eff4eba59becec52404e1c8f15b41440d6dd451
FreecouS_BG <63102871+Slavamk3dell@users.noreply.github.com>
2023-09-27 16:21:35 +0300

    sql

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,6 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите имя: ");
-var name = Console.ReadLine();
+﻿//using System.Data.SqlClient;
 
-Console.WriteLine($"Привет, {name}!!!");
-// я откатился
+using System.Data.SqlClient;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection connection = new SqlConnection(@"Server=ByashaLaptop\SQLEXPRESS, 49172;Database=Reckords;User ID=TEST2;Password=qwerty12345");
+
+            connection.Open();
+
+            var sqlQuery = $"SELECT TOP 10 * FROM Reckord where maptype = 1 ORDER BY Score ASC";
+            using SqlCommand command = new(sqlQuery, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+           
+            while (reader.Read())
+            {
+                string name = (string)reader["Nickname"];
+                int score = (int)reader["Score"];
+                DateTime date = (DateTime)reader["ScoreDate"];
+                Console.WriteLine($"Name = {name} score = {score} date = {date}");
+            }
+            reader.Close();
+        }
+    }
+}
